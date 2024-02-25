56352d368588b4632b404c1ae8ef9b7ae960a195
nitheesh0412 <nitheeshtangellapally4@gmail.com>
2024-02-26 00:05:07 +0530

    location dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,22 @@ userSubscription$ !: Subscription
 
   ngOnInit(): void {
     
+    (function (d, m) {
+      var kommunicateSettings = {
+        appId: "378cb90130214c38e06eada6e51aa1a03",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      (window as any).kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, (window as any).kommunicate || {});
+
 
     this.userSubscription$ = this._userdataservice.getUsers().subscribe((userData: User[]) => {
       this._userdataservice.allUsers = userData;
--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -5,6 +5,14 @@ import { Attendance } from 'src/app/models/attendance';
 import { User } from 'src/app/models/user';
 import { LocationService } from 'src/app/services/location.service';
 import { UserdataService } from 'src/app/services/userdata.service';
+interface DoctorDetails {
+  Hospital: string;
+  Name: string;
+  Specialization: string;
+  Consultation: number;
+  Experience: number;
+  // ... other properties
+}
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
@@ -14,14 +22,28 @@ export class LocationComponent implements OnInit {
   filter : string = ""
   latitude: number = 0;
   longitude: number = 0;
-  doctorDetails : any 
-  location(){
+  doctorDetails : any = {}
+  real : any
+  getObjectKeys(obj: any): string[] {
+    return Object.keys(obj);
+  }
 
+  location(){
+    console.log(this.filter)
+    
+    for(let key of this.getObjectKeys(this.doctorDetails)){
+      if(key.includes(this.filter)){
+        this.real = this.doctorDetails[key]
+        console.log(this.real)
+      }
+    }
+    this.getLocation();
   }
   constructor(private _locationService : LocationService){
 
   }
   ngOnInit(): void {
+    
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -37,13 +59,22 @@ export class LocationComponent implements OnInit {
       console.error('Geolocation is not supported by this browser.');
     }
   }
+  keys : string[] = ["kukatpally", "miyapur", "lb nagar", "manikonda" , "madhapur" , "hayathnagar"]
   getLocation() {
     
-    this._locationService.getVetByLatLon(this.latitude,this.longitude).subscribe(data => {
-      console.log(data);
-      this.doctorDetails = data;
-      console.log(this.doctorDetails.message.slice(0,5  ))
+    this._locationService.getVetByLatLon(this.latitude,this.longitude).subscribe((data : any) => {
+      // console.log(data);
+      this.doctorDetails = {
+        'kukatpally': data.message.slice(0, 5),
+        'miyapur': data.message.slice(5, 10),
+        'lb nagar': data.message.slice(0, 5),
+        'manikonda': data.message.slice(5, 10),
+        'madhapur': data.message.slice(0, 5),
+        'hayathnagar': data.message.slice(5, 10),
+      };
     });
   }
-
+  matchesFilter(key: string): boolean {
+    return key.toLowerCase().includes(this.filter.toLowerCase());
+  }
 }
